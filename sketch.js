9b2edb279ce68c0c4265fb4cd1d3ced60c6c0341
joana maria de jesus souza <98822804+JoanaMariaDeJesusSouza1@users.noreply.github.com>
2022-03-25 20:39:03 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+
+var box;
+
+
+function setup() {
+  createCanvas(600,600);
+  
+box=createSprite(300,300,25,25);
+box.shapeColor="purple";
+
+}
+
+function draw() 
+{
+  background(0);
+
+  if(keyIsDown(UP_ARROW)){
+      box.y=box.y -5;
+  }
+
+  if(keyIsDown(DOWN_ARROW)){
+    box.y=box.y +5;
+      }
+      if (keyDown("space")) {
+       background("green") 
+      }
+    
+  drawSprites();
+}
+
+
+
+
